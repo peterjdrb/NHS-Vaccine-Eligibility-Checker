8fb9ec3c6dae41af1a9edf163e750eea812794a4
Peter Robertson <peterjdrb@users.noreply.github.com>
2021-05-12 21:01:26 +0100

    you can now click on notification to send you to the nhs website

--- src/check.js
+++ src/check.js
@@ -15,37 +15,39 @@ module.exports.NHSVaccine = async () => {
     await page.goto('https://www.nhs.uk/book-a-coronavirus-vaccination/do-you-have-an-nhs-number', { timeout: 30000, waitUntil: 'domcontentloaded' });
     await page.click('input[id=option_No_input]');
     await page.keyboard.press('Enter')
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
 
     //Enter Name
     await page.type('input[id=Firstname]', data.firstName);
     await page.type('input[id=Surname]', data.surname);
     await page.keyboard.press('Enter')
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
 
     //Enter date of birth
     await page.type('input[id=Date_Day]', data.dob.day);
     await page.type('input[id=Date_Month]', data.dob.month);
     await page.type('input[id=Date_Year]', data.dob.year);
     await page.keyboard.press('Enter')
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
 
     //Enter postcode
     await page.type('input[id=Postcode]', data.postcode);
     await page.keyboard.press('Enter')
-    await page.waitForNavigation({waitUntil: 'networkidle2'});
+    await page.waitForNavigation({waitUntil: 'networkidle0'});
 
-    //Print Title
+    //Get page text to see if they are eligible
     const heading = await page.evaluate( () => {
-    const dom = document.querySelector('h1')
-    return dom.innerText
+        const dom = document.querySelector('h1')
+        return dom.innerText
     })
+    await browser.close();
 
     if (heading === 'You are not currently eligible to book through this service') {
         notifier.notify({
             title: 'Not yet :(',
             message: "You're not quire ready to get your vaccine just yet"
           });
+
     } else {
         notifier.notify({
             title: 'Breaking News!',
@@ -53,5 +55,8 @@ module.exports.NHSVaccine = async () => {
           });
     }
 
-    await browser.close();
+    notifier.on('click', function (notifierObject, options, event) {
+        require("openurl").open("https://www.nhs.uk/book-a-coronavirus-vaccination/do-you-have-an-nhs-number")
+      });
+
 }; 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const check = require('./check')
 
-const sleep = seconds => new Promise(resolve => setTimeout(resolve, seconds*1000));
-
 const main = async() => {
     await check.NHSVaccine()
 }
